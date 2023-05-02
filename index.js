2066424647fac1ba2cfaa8affbd887db8f47f488
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-05-03 01:14:16 +0600

    all chef and single chef data url made

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const chefData = require('./chefData.json');
 
+
+const port = process.env.PORT || 5000
 const cors = require('cors')
 
 app.use(cors());
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/chef-data', (req, res) =>{
+  res.send(chefData);
+})
+
+app.get("/chef-data/:id", (req, res) =>{
+  const id = req.params.id;
+  
+  const data = chefData.find(d => d.id == id);
+  console.log(id)
+
+  res.send(data);
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
